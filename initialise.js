46a3a57154aa3b7a343c3489b35f0726abc54d01
MitchLloyd <mitchelllloyd154@gmail.com>
2024-08-28 16:37:34 +0000

    check can initialise db

---
+++ initialise.js
@@ -0,0 +1,12 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const { setupDB, removeTables } = require("./dbSetup.js");
+
+(async () => {
+  const db = await open({
+    filename: "todo.sqlite",
+    driver: sqlite3.Database,
+  });
+  await removeTables(db);
+  await setupDB(db, {todos: [{ email_address: "mitchell@ada.ac.uk", email: 'geoff@ada.ac.uk' }], users: [{ email_address: "claire@ada.ac.uk" }]});
+})();
