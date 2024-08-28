1258a65ac1358a7a0c192c53d0dbe09e144684f8
MitchLloyd <mitchelllloyd154@gmail.com>
2024-08-28 16:36:04 +0000

    add promisified setup db

--- dbSetup.js
+++ dbSetup.js
@@ -1,18 +1,46 @@
-const sqlite3 = require('sqlite3').verbose()
-
-function dbSetup() {
-  let db = new sqlite3.Database("todo.sqlite");
-  
-  db.run( // Insert query below
-    `
-    `);
-
-
-  db.run( // Insert query below
-    `
-    `);
-
-  db.close()
+function buildInsertString(data) {
+  const fields = `(${Object.keys(data[0])})`;
+  const values = data.reduce((acc, item,i) => {
+    return acc + `('${Object.values(item)}')` + (i < data.length - 1 ? ',' : '');
+  }, "");
+  return [fields, values];
 }
 
-dbSetup()
+async function setupDB(db, { todos, users }) {
+  const [userFields, userValues] = buildInsertString(users);
+  const [todoFields, todoValues] = buildInsertString(todos);
+  await db.exec(
+    `
+    CREATE TABLE users (
+      email_address varchar,
+      first_name varchar,
+      last_name varchar,
+      notification_ind char
+    );
+    INSERT INTO users ${userFields} VALUES ${userValues};
+    `,
+  );
+  await db.exec(
+    `
+    create table todos (
+      email_address varchar,
+      todo_id int,
+      priority varchar,
+      archive_ind char,
+      created_dt timestamp
+    );
+    INSERT INTO todos ${todoFields} VALUES ${todoValues};
+      `,
+  );
+}
+
+async function removeTables(db) {
+  const rawDb = db.getDatabaseInstance();
+  await rawDb.serialize();
+  const tables = await db.all(
+    "SELECT name FROM sqlite_master WHERE type='table'",
+  );
+  await Promise.all(tables.map((table) => db.run(`DROP TABLE ${table.name}`)));
+}
+
+module.exports = { setupDB, removeTables };
