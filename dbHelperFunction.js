3aac282a8179f5fa7a053930e17ce0965d98ca18
MitchLloyd <mitchelllloyd154@gmail.com>
2024-08-28 18:10:32 +0000

    implement insertUser

--- dbHelperFunction.js
+++ dbHelperFunction.js
@@ -1,17 +1,14 @@
-const e = require("express");
-
 const sqlite3 = require("sqlite3").verbose();
 
 class SQL {
-  constructor(database = "todo.sqlite") {
-    this.db = new sqlite3.Database(database);
+  constructor(db) {
+    this.db = db;
   }
 
   async insertUser(emailAddress, firstName, lastName, notificationInd) {
-    console.log({emailAddress, firstName, lastName, notificationInd},'inside insertUser...');
     await this.db.run(
       `
-      INSERT INTO users (email_address,first_name, last_name, notification_ind) VALUES ('${emailAddress}','${firstName}','${lastName}','${notificationInd} returning *;');
+      INSERT INTO users (email_address,first_name, last_name, notification_ind) VALUES ('${emailAddress}','${firstName}','${lastName}','${notificationInd}');
       `,
     );
   }
