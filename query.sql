a88b5523e7414722e1ec3e2cca729ea1ad1642c7
MitchLloyd <mitchelllloyd154@gmail.com>
2024-08-29 17:01:47 +0000

    query for users and todos

--- query.sql
+++ query.sql
@@ -14,3 +14,8 @@ from todos
 right join users
 on users.email_address = todos.email_address 
 GROUP BY users.email_address;
+
+-- select all users and todos
+select first_name, content, title from todos
+full join users
+on users.email_address = todos.email_address;
