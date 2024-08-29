6976bb881a34827edebac1b92fc39bc179a892e7
MitchLloyd <mitchelllloyd154@gmail.com>
2024-08-29 15:13:48 +0000

    check queries

---
+++ query.sql
@@ -0,0 +1,16 @@
+
+select count(todos.id) as total_todos
+from todos
+right join users
+on users.email_address = todos.email_address 
+GROUP BY users.email_address;
+
+
+select * from todos;
+
+-- find max number of todos via email
+select max(todos.id) as total_todos
+from todos
+right join users
+on users.email_address = todos.email_address 
+GROUP BY users.email_address;
