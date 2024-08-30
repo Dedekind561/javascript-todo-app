4e5fcebd0d7dc83d8d6c4e2d1499ad3cedeb16ad
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-08-30 16:30:51 +0100

    format file

--- experiment.js
+++ experiment.js
@@ -1,28 +1,28 @@
 const users = [
-  { first_name: "Steve", content: "Do homework", title: "Homework" , email_address: 'steve@ada.ac.uk'},
+  { first_name: "Steve", content: "Do homework", title: "Homework", email_address: "steve@ada.ac.uk" },
   {
     first_name: "Geoff",
     content: "Organise meeting",
     title: "Meetings",
-    email_address: 'geoff@ada.ac.uk'
+    email_address: "geoff@ada.ac.uk",
   },
   {
     first_name: "Geoff",
     content: "Plan staff training",
     title: "Planning",
-    email_address: 'geoff@ada.ac.uk'
+    email_address: "geoff@ada.ac.uk",
   },
   {
     first_name: "Geoff",
     content: "Interview candidate lecturer",
     title: "Interviews",
-    email_address: 'geoff@ada.ac.uk'
+    email_address: "geoff@ada.ac.uk",
   },
-  { first_name: "Claire", content: null, title: null , email_address: 'claire@ada.ac.uk'},
+  { first_name: "Claire", content: null, title: null, email_address: "claire@ada.ac.uk" },
 ];
 
 function groupUsers(users) {
-  const groupedUsers =  users.reduce((acc, user) => {
+  const groupedUsers = users.reduce((acc, user) => {
     if (!acc[user.first_name]) {
       acc[user.first_name] = [user.content];
     } else {
@@ -35,5 +35,3 @@ function groupUsers(users) {
     return { user, todos };
   });
 }
-
-console.log(groupUsers(users))
