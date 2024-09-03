9e127ab47e27505113e26a411bd78e924bb829a7
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-09-03 12:25:34 +0100

    setup sql files for part 1

---
+++ part-1/setup.sql
@@ -0,0 +1,8 @@
+.mode csv
+.open test_todos.sqlite
+
+-- create tables your here and check they've been created
+
+-- check you can import sample data into the tables you've created to verify your SQL statements are working
+-- .import ./users.csv users
+-- .import todos.csv todos
